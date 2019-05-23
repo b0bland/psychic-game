1ef64ec8873a7386d4758ab8f136a0ed5c1a1e46
b0bland <alfredychan96@gmail.com>
2019-05-22 22:55:07 -0700

    Added some variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,6 @@
 var letters = ["q","w","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
 
+var win = 0;
+var loss = 0;
+var guess;
+var gameon = true;
