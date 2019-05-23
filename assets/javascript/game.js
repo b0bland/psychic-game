ce4c4913fc66d23e4f8d5c764b36f1458188a0d0
b0bland <alfredychan96@gmail.com>
2019-05-22 23:42:47 -0700

    Added mechanism for wrong guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,23 +3,31 @@ var letters = ["q","w","w","e","r","t","y","u","i","o","p","a","s","d","f","g","
 var win = 0;
 var loss = 0;
 var playerguess;
+var playerguessarr = [];
 var comguess;
-var tries = 0;
+var tries = 10;
 
 comguess = letters[Math.round(Math.random() * (letters.length - 1))]
+document.getElementById("remain").textContent += ("Guesses left: " + tries);
 
 document.onkeypress = function(input) {
     playerguess = input.key.toLowerCase();
-    if (tries < 10) {
+    if (tries > 0) {
         if (playerguess === comguess) {
             alert("You have guessed the right letter!");
             win++;
-            tries = 0;
+            tries = 10;
             document.getElementById("wins").textContent = ("Wins: " + win);
             document.getElementById("remain").textContent = ("Guesses left: 10");
             document.getElementById("guess").textContent = ("Your guesses so far:");
+            document.getElementById("notif").textContent = (" ");
             comguess = letters[Math.round(Math.random() * (letters.length - 1))];
-            console.log(comguess)
+        }
+        else if (playerguess != comguess) {
+            document.getElementById("guess").textContent += (" " + playerguess)
+            document.getElementById("notif").textContent = ("Your guess is incorrect, try again!");
+            tries--;
+            document.getElementById("remain").textContent = ("Guesses left: " + tries);
         }
     }
 }
