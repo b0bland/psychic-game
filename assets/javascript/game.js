18747a43aee4bd871cdf206ae7825b862a52c6bf
b0bland <alfredychan96@gmail.com>
2019-05-22 23:12:05 -0700

    Added mechanism for correct guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,13 +4,24 @@ var win = 0;
 var loss = 0;
 var playerguess;
 var comguess;
-var gameon = true;
+var tries = 0;
 
 comguess = letters[Math.round(Math.random() * (letters.length - 1))]
 
 document.onkeypress = function(input) {
     playerguess = input.key.toLowerCase();
-    
-    console.log(comguess)
+    if (tries < 10) {
+        if (playerguess === comguess) {
+            alert("You have guessed the right letter!");
+            win++;
+            tries = 0;
+            document.getElementById("wins").textContent = ("Wins: " + win);
+            document.getElementById("remain").textContent = ("Guesses left: 10");
+            document.getElementById("guess").textContent = ("Your guesses so far:");
+            comguess = letters[Math.round(Math.random() * (letters.length - 1))];
+            console.log(comguess)
+        }
+    }
 }
 
+console.log(comguess)
